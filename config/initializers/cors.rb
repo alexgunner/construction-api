85bc7eb3e541c01f7fc174c3aff56b9123e0c9e1
Alexander <a.peredo@expansesoft.com>
2018-05-17 16:36:08 -0400

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://boiling-inlet-44438.herokuapp.com/*'
     resource '*', :headers => :any, :methods => [:get, :post, :patch, :update, :options]
   end
 end
