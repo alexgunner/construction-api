a7c7b2e90cf0c9f72b315cb418d62b33065175cc
Alexander <a.peredo@expansesoft.com>
2018-05-17 16:33:53 -0400

    fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :update, :destroy]
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   # GET /products
   def index
